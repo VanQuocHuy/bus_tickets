92a85666a9b251e768bde37b39a428cbf7bb7237
unknown <levanquochuy06022001@gmail.com>
2024-09-06 13:34:01 +0700

    upload code

--- pages/book_tickets.vue
+++ pages/book_tickets.vue
@@ -40,15 +40,95 @@
          </div>
          <button class="search">Tìm chuyến xe </button>
       </div>
+
       <div class="Find-trip">
         <div class="search-filter">
             <div class="filter">
-                BỘ LỌC TÌM KIẾM 
+              BỘ LỌC TÌM KIẾM
             <button class="clear-filter-button">
-                Bỏ lọc
+              Bỏ lọc
              <img src="/images/delete.svg" alt="Clear">
             </button>
             </div>
+            <div class="time">
+                <p class="test">Giờ đi </p>
+                <div class="times">
+                  <label>
+                    <input type="checkbox" value="morning">
+                      Sáng sớm 00:00 - 06:00 (0)
+                  </label>
+                  <label>
+                    <input type="checkbox" value="morning">
+                      Buổi sáng 06:00 - 12:00 (3)
+                  </label>
+                  <label>
+                    <input type="checkbox" value="afternoon">
+                      Buổi chiều 12:00 - 18:00 (1)
+                  </label>
+                  <label>
+                    <input type="checkbox" value="evening">
+                      Buổi tối 18:00 - 24:00 (2)
+                  </label>
+                </div>
+            </div>
+            <hr class="lane">
+            <div class="bus-car">
+              <p class="test">Loại xe</p>
+              <div class="car">
+                <button class="car-bus"> Ghế </button>
+                <button class="car-bus"> Giường </button>
+                <button class="car-bus"> Limousine </button>
+              </div>
+            </div>
+            <hr class="lane">
+            <div class="chair">
+              <p class="test">Hàng ghế</p>
+              <div class="car">
+                <button class="row-chair"> Hàng đầu </button>
+                <button class="row-chair"> Hàng giữa </button>
+                <button class="row-chair"> Hàng cuối </button>
+              </div>
+            </div>
+            <hr class="lane">
+            <div class="Floor-bus">
+              <p class="test">Tầng</p>
+              <div class="car">
+                <button class="Floor"> Tầng trên </button>
+                <button class="Floor"> Tầng dưới </button>
+              </div>
+            </div>
+        </div>
+        <div class="list-bus">
+          <div class="lists">
+            Đà Nẵng - TP.Hồ Chí Minh (5)
+          </div>
+          <div class="in4-bus">
+            <div  class="bus-details">
+            <span class="timegoto">17:15</span>
+              <img src="/images/go.svg" alt="Điểm đi" class="icongoto">
+            <span class="dots">.................</span>
+            <span class="duration">15 giờ</span>
+            <span class="dots">.................</span>
+              <img src="/images/to.svg" alt="Điểm đến" class="icongoto">
+            <span class="timegoto">08:15</span>
+            <span class="seats">Giường: 31 chổ trống</span>
+            </div>
+            <div class="bus-locations">
+              <div class="bus-location">Bến Xe Trung Tâm Đà Nẵng</div>
+              <div class="buslocation">Bến Xe Trung Tâm TP.Hồ Chí Minh</div>
+           </div>
+           <hr class="lane">
+           <div class="btn-click">
+            <div class="click-btn">
+              <button class="btnclick">Trung chuyển</button>
+              <button class="btnclick">Chính Sách</button>
+            </div>
+            <div>
+              <NuxtLink to="/confirm" class="select-flight"> Chọn chuyến </NuxtLink>
+              <!-- <button class="select-flight">Chọn chuyến</button> -->
+            </div>
+           </div>
+          </div>
         </div>
       </div>
    </div>
@@ -89,10 +169,169 @@
 </script>
    
 <style>
+.select-flight {
+   width: 150px;
+   height: 35px;
+   border: none;
+   background-color: rgb(255, 184, 158);
+   border-radius: 25px;
+   color: #f94d0a;
+   font-weight: bold;
+   align-self: center;
+   font-size: 16px;
+   display: flex; 
+   justify-content: center;
+   align-items: center; 
+   text-decoration: none;
+}
+.select-flight:hover {
+  color: white;
+  background-color: #f94d0a;
+}
+.btn-click {
+  display: flex;
+  align-items: center;
+  justify-content: space-between; 
+  margin-top: 10px;
+}
+.btnclick{
+  border: none;
+  background-color: white;
+  color: #f94d0a;
+  font-weight: bold;
+}
+.buslocation{
+  margin-top: 3px;
+  margin-left: 20%;
+}
+.bus-locations {
+  display: flex;
+  margin-top: 10px; 
+  margin-left: 10px;
+}
+.bus-location {
+  font-size: 14px; 
+  margin-top: 5px;
+}     
+.bus-details {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.seats {
+  margin-left: 50px; 
+  font-size: 16px;
+  color: #015000; 
+}
+.duration {
+  color: #919191;
+  margin: 0 10px; 
+}
+.dots {
+  flex-grow: 1;
+  text-align: center;
+  font-size: 16px;
+  margin: 0 10px; 
+}
+.icongoto {
+  width: 16x; 
+  height: 16px;
+  margin: 0 15px; 
+}
+.timegoto {
+  font-size: 25px;
+  min-width: 80px; 
+  text-align: center;
+  
+}
+.in4-bus{
+  border: 1px solid rgb(250, 78, 44);
+  margin-top: 3%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 3%;
+  padding: 10px;
+  font-size: 16px;
+  width: 107%;
+  border-radius: 15px;
+  box-shadow: 0 4px 8px rgba(249, 77, 10, 0.4);
+} 
+.lists{
+  display: flex;
+  align-items: center;
+  margin-top: 1.5%;
+}
+.test{
+  font-weight: bold;
+}
+.Floor{
+  border: 1px solid rgb(152, 152, 152);
+  border-radius: 5px;
+  margin-right: 7px;
+  background-color: #f9f9f9;
+  margin-bottom: 5%;
+}
+.Floor-bus{
+  display: flex;
+  flex-direction: column;
+  margin-left: 5%;
+  margin-top: 5%;
+}
+.row-chair{
+  
+  border: 1px solid rgb(152, 152, 152);
+  border-radius: 5px;
+  margin-right: 7px;
+  background-color: #f9f9f9;
+}
+.chair{
+  display: flex;
+  flex-direction: column;
+  margin-left: 5%;
+  margin-top: 5%;
+}
+.car-bus{
+  border: 1px solid rgb(152, 152, 152);
+  border-radius: 5px;
+  margin-right: 7px;
+  background-color: #f9f9f9;
+}
+.bus-car{
+  display: flex;
+  flex-direction: column;
+  margin-left: 5%;
+  margin-top: 5%;  
+}
+.times input[type="checkbox"]:hover {
+    accent-color: #f94d0a; 
+}
+.times label:hover {
+    color:#f94d0a; 
+}
+.times input[type="checkbox"] {
+  accent-color: #f94d0a;
+  margin-right: 10px; 
+}
+.times label {
+  display: flex;
+  align-items: center;
+  margin-top: 10px; 
+}
+.times {
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px; 
+}
+.time{
+  display: flex;
+  flex-direction: column;
+  margin-left: 5%;
+  margin-top: 7%;  
+}
 .clear-filter-button img {
-  width: 20px; /* Kích thước của icon */
+  width: 20px; 
   height: 20px;
-  margin-left: 5px; /* Khoảng cách giữa text "Bỏ lọc" và icon */
+  margin-left: 5px;
 }
 .clear-filter-button {
   background-color: transparent; 
@@ -111,7 +350,6 @@
 }
 .Find-trip{
  margin-top: 5%;
- border: 1px solid red; 
  padding: 10px;
  margin: 10px auto;
  border-radius: 10px;
@@ -119,14 +357,13 @@
  margin-top: 30px;
  display: flex;
  gap: 25px; 
- background-color: #f9f9f9;
+ background-color: white;
 }
 .search-filter{
   border: 1px solid black;
   border-radius: 10px;
   width: 30%;
 }
-
 .search {
    width: 250px;
    height: 45px;
@@ -199,7 +436,7 @@
        margin-left: auto;
        font-weight: bold;
    }
-   .options-box {
+   .options-box{
        border: 1px solid red; 
        padding: 10px;
        margin: 10px auto;
@@ -230,13 +467,13 @@
        transform: translateX(-50%);
        width: 100%;
        z-index: 1; 
-   }
-   .banner-img {
+}
+.banner-img {
        width: 100%; 
        height: auto; 
        max-height: 400px; 
        object-fit: cover; 
        border-radius: 5px;
-   }
+}
      </style>
    
---
+++ pages/confirm.vue
@@ -0,0 +1,438 @@
+<template>
+  <div class="container-confirm">
+    <div class="confirm-ctn">
+      <!-- <div class="icon-back" @click="goBack">
+      <img src="/images/back.svg" alt="icon-back" class="back">
+        </div> -->
+      <div class="choose-seat">
+        <div class="chooseseat">
+        <div class="choose">
+           <h3>Chọn ghế</h3>
+        </div>
+        <hr class="lane">
+        <div class="floor-seat">
+          <div class="downstairs">
+               Tầng dưới
+            <div class="below">
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 1">
+                <span class="seat-text">A01</span>
+              </div>
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 2">
+                <span class="seat-text">A02</span>
+              </div>
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 3">
+                <span class="seat-text">A03</span>
+              </div>
+            </div>
+            <div class="below">
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 4">
+                <span class="seat-text">A04</span>
+              </div>
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 5">
+                <span class="seat-text">A05</span>
+              </div>
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 6">
+                <span class="seat-text">A06</span>
+              </div>
+            </div>
+            <div class="below">
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 7">
+                <span class="seat-text">A07</span>
+              </div>
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 8">
+                <span class="seat-text">A08</span>
+              </div>
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 9">
+                <span class="seat-text">A09</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="upper-floor">
+            Tầng trên
+            <div class="below">
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 1">
+                <span class="seat-text">B01</span>
+              </div>
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 2">
+                <span class="seat-text">B02</span>
+              </div>
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 3">
+                <span class="seat-text">B03</span>
+              </div>
+            </div>
+            <div class="below">
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 4">
+                <span class="seat-text">B04</span>
+              </div>
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 5">
+                <span class="seat-text">B05</span>
+              </div>
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 6">
+                <span class="seat-text">B06</span>
+              </div>
+            </div>
+            <div class="below">
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 7">
+                <span class="seat-text">B07</span>
+              </div>
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 8">
+                <span class="seat-text">B08</span>
+              </div>
+              <div class="seat-item">
+                <img src="/images/ghe.svg" alt="Ghế 9">
+                <span class="seat-text">B09</span>
+              </div>
+            </div>
+          </div>
+          <div class="ticks-seat">
+            <div class="done-tick">
+             <div class="donetick">
+              <img src="/images/ghe2.svg" alt="Ghế đã chọn" class="seat-image">
+              <span class="seat-selected">Đã đặt</span>
+             </div>
+             <div class="donetick">
+              <img src="/images/ghe.svg" alt="Ghế đã chọn" class="seat-image">
+              <span class="seat-selected">Còn trống</span>
+             </div>
+             <div class="donetick">
+              <img src="/images/ghe1.svg" alt="Ghế đã chọn" class="seat-image">
+              <span class="seat-selected">Đang chọn</span>
+             </div>
+          </div>
+         </div>
+        </div>
+      </div>
+      <div class="infor4">
+        <div class="customer-in4">
+          <p>Thông tin khách hàng</p>
+          <div class="in4">
+            <label for="full-name">Họ và tên:</label>
+             <input type="text" id="full-name" placeholder="Nhập họ và tên" class="inputfield">
+  
+            <label for="phone-number">Số điện thoại:</label>
+             <input type="tel" id="phone-number" placeholder="Nhập số điện thoại" class="inputfield">
+  
+            <label for="email">Email:</label>
+             <input type="email" id="email" placeholder="Nhập email" class="inputfield">
+          </div>
+        </div>
+        <div class="note">
+          <p class="P1">ĐIỀU KHOẢN & LƯU Ý</p>
+          <div class="clause">
+            <p>(*) Quý khách vui lòng có mặt tại bến xuất phát của xe trước ít nhất 30 phút giờ xe khởi hành, 
+              mang theo thông báo đã thanh toán vé thành công có chứa mã vé được gửi từ hệ thống C-MART BUS LINE. 
+              Vui lòng liên hệ Trung tâm tổng đài <span style="color: red;">1900 6067 </span>để được hỗ trợ.</p>
+            <p>(*) Nếu quý khách có nhu cầu trung chuyển, vui lòng liên hệ Tổng đài trung chuyển <span style="color: red;">1900 6918</span> 
+              trước khi đặt vé. Chúng tôi không đón/trung chuyển tại những điểm xe trung chuyển không thể tới được.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="pick-drop">
+        <div class="in4-pickdrop">
+          <p class="P">Thông tin đón trả </p>
+          <div class="don-tra">
+            <div class="pick">
+              <span style="color: red; font-weight: bold;">ĐIỂM ĐÓN</span>
+              <input type="text" value="Bến xe Trung tâm Đà Nẵng" readonly>
+              <div class="luu-y">
+                <p>Quý khách vui lòng có mặt tại Bến xe/Văn Phòng
+                  <span style="font-weight: bold;">Đà Nẵng</span> <span style="color: red; font-weight: bold;">Trước 17:00 06/09/2024</span>
+                  để được trung chuyển hoặc kiểm tra thông tin trước khi lên xe.
+                </p>
+              </div>
+            </div>
+            <div class="divider"></div>
+            <div class="drop">
+              <span style="color: red; font-weight: bold;">ĐIỂM TRẢ</span>
+              <input type="text" value="Bến xe Đắk Nông" readonly>
+            </div>
+          </div>
+        </div>
+        <div class="pay">
+          <button class="thanhtoan">Thanh toán</button>
+        </div>
+      </div>
+    </div>
+      
+      <div class="seat">
+        <div class="in4-trip">
+          <div class="in4trip">
+            <h3>Thông tin lượt đi</h3>
+          </div>
+          <hr class="lane">
+          <div>
+            123
+          </div>
+        </div>
+        <div class="price">
+          <div class="gia">
+            <h3>Chi tiết giá</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.gia h3{
+  margin-top: 3.5%;
+  color: red;
+}
+.price{
+  text-align: center;
+  border: 1px solid rgb(255, 164, 164);
+  margin-top: 4%;
+  border-radius: 10px;
+}
+.in4-trip{
+  text-align: center;
+  border-radius: 10px;
+  border: 1px solid rgb(255, 164, 164);
+  box-shadow: 0 4px 8px rgba(249, 77, 10, 0.4);
+}
+.in4-trip h3{
+  margin-top: 3.5%;
+  color: red;
+}
+.seat{
+  width: 35%;
+}
+.thanhtoan{
+  border: none;
+  background-color: #f2754e;
+  margin-bottom: 2%;
+  border-radius: 15px;
+  width: 130px;
+  color: white;
+  justify-content: center;
+  height: 35px;
+}
+.pay{
+  margin-top: 3%;
+}
+.P{
+  color: red;
+  font-weight: bold;
+}
+input[readonly] {
+  background-color: #f9f9f9;
+  border: 1px solid #ccc;
+  padding: 10px;
+  border-radius: 15px;
+  font-size: 14px;
+  width: 80%;
+  margin-top: 5px;
+  color: #333;
+}
+.drop{
+  margin-left: 3%;
+  text-align: center;
+  width: 50%;
+}
+.divider {
+  width: 1px;
+  height: 100%; 
+  background-color: #c2c2c2; 
+}
+.pick{
+  margin-left: 3%;
+  text-align: center;
+  width: 50%;
+}
+.don-tra{
+  display: flex;
+  justify-content: space-between;
+  gap: 20px;
+  height: 190px;
+}
+.in4-pickdrop p{
+  margin-top: 2%;
+}
+.pick-drop{
+  border: 1px solid rgb(255, 164, 164);
+  width: 100%;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(249, 77, 10, 0.4);
+  margin-top: 2%;
+}
+.clause{
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 20px;
+  text-align: left; 
+}
+.note{
+  text-align: center;
+  width: 50%;
+  margin-top: 2%;
+}
+.P1 {
+  color: rgb(255, 54, 54);
+  font-weight: bold;
+}
+.in4 {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 20px;
+  text-align: left;
+}
+.in4 label {
+  font-weight: bold;
+}
+.inputfield {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  font-size: 14px;
+  box-shadow: 0 2px 4px rgb(252, 110, 94);
+}
+.inputfield:hover {
+  border: 1px solid #fe7861;
+}
+.customer-in4{
+  width: 50%;
+  text-align: center;
+  margin-top: 2%;
+}
+.customer-in4 p{
+  color: rgb(255, 54, 54);
+  font-weight: bold;
+}
+.chooseseat{
+  border: 1px solid rgb(255, 164, 164);
+  width: 100%;
+  border-radius: 10px;
+  text-align: center;
+  box-shadow: 0 4px 8px rgba(249, 77, 10, 0.4);
+}
+.infor4{
+  margin-top: 2%;
+  border: 1px solid rgb(255, 164, 164);
+  width: 100%;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(249, 77, 10, 0.4);
+  display: flex;
+}
+.done-tick{
+  margin-top: 30%;
+}
+.donetick img {
+  margin-right: 10px; 
+}
+.donetick{
+  display: flex;
+  align-items: center; 
+  margin-bottom: 20px;
+}
+.ticks-seat{
+  width: 35%;
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+}
+.upper-floor{
+  width: 35%;
+  font-weight: bold;
+}
+.seat-item img {
+  width: 50%;
+  height: 50%;
+  object-fit: cover;
+  cursor: pointer;
+}
+.seat-text {
+  position: absolute;
+  top: 25%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: rgb(0, 0, 0);
+  font-size: 14px;
+  font-weight: bold;
+  text-align: center;
+  cursor: pointer;
+}
+.seat-item {
+  position: relative;
+  display: inline-block;
+  width: 80px;
+  height: 80px;
+}
+.below {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
+  margin-top: 7%;
+}
+.downstairs{
+  width: 35%;
+  font-weight: bold;
+}
+.floor-seat{
+  display: flex;
+  margin-top: 3%;
+}
+
+.choose-seat{
+  width: 65%;
+  text-align: center;
+  /* border: 1px solid rgb(164, 165, 255); */
+}
+.choose-seat h3{
+  margin-top: 2%;
+  color: red;
+}
+.icon-back {
+  position: absolute;
+  background-color: rgb(255, 255, 255);
+  border: none;
+  cursor: pointer;
+  height: 30px;
+  left: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  margin-top: -5px;
+}
+.confirm-ctn{
+  padding: 10px;
+  margin: 10px auto;
+  border-radius: 10px;
+  width: 1150px;
+  margin-top: 30px;
+  display: flex;
+  gap: 25px; 
+  background-color: white; 
+  
+}
+
+
+</style>
